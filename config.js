36226d3c1cda86dd03f593cdd908863b0f42cbfd
Ruchi Gupta <70604743+ruchi-gupta94@users.noreply.github.com>
2021-04-01 08:05:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-import * as firebase from 'firebase';
+import firebase from 'firebase';
 require('@firebase/firestore');
 
 var firebaseConfig = {
@@ -11,7 +11,7 @@ var firebaseConfig = {
 };
   
 // Initialize Firebase
-if(!firebase.apps.len1){
+if(!firebase.apps.length){
   firebase.initializeApp(firebaseConfig);
 }
 
